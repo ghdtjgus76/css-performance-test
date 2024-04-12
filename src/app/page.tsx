2b2624845dd37bf45c8c86da3c176adf48b39b2b
ghdtjgus76 <ghdtjgus76@naver.com>
2024-04-13 00:50:55 +0900

    feat: 초기 세팅 및 파일 작성

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import { css } from "../../styled-system/css";
+
+export default function Home() {
+  return (
+    <main className={css({ backgroundColor: "lightblue", padding: 20 })}>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+      <h1 className={css({ color: "blue", fontSize: 24, fontWeight: 500 })}>
+        css 렌더링 테스트
+      </h1>
+    </main>
+  );
+}
